31f9941c178dcb117b9dc6221d7f07d07b67c710
Trevor Foehl <trevorfoehl@gmail.com>
2021-01-29 21:51:14 -0500

    Added a modifier calculator and began race bonuses

--- src/App.js
+++ src/App.js
@@ -5,81 +5,50 @@ import {
 import { AwesomeButton } from "react-awesome-button";
 import Dropdown from 'react-bootstrap/Dropdown'
 
+import races from './races.json';
+
 import logo from './logo.svg';
 
 import './App.css';
 import "react-awesome-button/dist/styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// function CounterFunc(props) {
-//   const [ count, setCount ] = useState(props.value);
-//   return (
-//     <div>
-//       <h1>You've clicked the button {count} times!</h1>
-//       {props.children}
-//       <button onClick={() => setCount(count + 1)}>Increment!</button>
-//       <button onClick={() => setCount(count - 1)}>Decrement!</button>
-//     </div>
-//   )
-// }
+// 1. Let people choose their race
+// 2. Let people choose sub-race
+// 3. Let people choose their "wildcard" attrs (if available)
+// 4. Show people their correct modifier
 
-// CounterFunc.Title = function Title(props) {
-//   return <p className="fs-1">{props.children}</p>;
-// }
-
-// class CounterButton extends Component {
-//   constructor(props) {
-//     super();
-//     this.state = {
-//       count: props.value
-//     };
-//   }
-
-//   increment() {
-//     this.setState( {
-//       count: this.state.count + 1
-//     })
-//   }
-
-//   decrement() {
-//     this.setState( {
-//       count: this.state.count - 1
-//     })  
-//   }
-
-//   render(props) {
-//     return <div>
-//       <h1>You've clicked the button {this.state.count} times!</h1>
-//       <button onClick={() => this.decrement()}>Decrement</button>
-//       <button onClick={() => this.increment()}>Increment</button>
-//     </div>
-//   }
-// }
+function calcModifier(atb) {
+  if (atb < 3) { return ""; }
+  return Math.floor((atb - 10) / 2);
+}
 
 function Attribute(props) {
- const [ atb, setAtb ] = useState(0) 
- 
+  const [ atb, setAtb ] = useState(0) 
+
   return <div>
     <Dropdown>
       <Dropdown.Toggle variant="success" id="dropdown-basic" className="attribute">
-        {props.name}
+        {props.name} ({atb} + {props.bonus} = {atb + props.bonus})
+        
       </Dropdown.Toggle>
       <Dropdown.Menu>
         {props.options.map((value) => {
           return <Dropdown.Item onClick={() => { props.onSelect(atb, value); setAtb(value); }}>{value}</Dropdown.Item>
         })}
+        <Dropdown.Item onClick={() => { props.onSelect(atb, 0); setAtb(0); }}>Clear</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
 
-    <h2>{atb}</h2>
+    <h2>Modifier: {calcModifier(atb + props.bonus)}</h2>
  </div> 
 }
 
-function App() {
-  const [ stdArr, setStdArr ] = useState([ 15, 14, 13, 12, 10, 8 ]);
-  function claimValue(oldValue, newValue) {
 
-    // 1. Remove newValue from stdArr
+
+function App() {
+  const [ stdArr, setStdArr ] = useState([ 15, 14, 13, 12, 10, 8, ]);
+  function claimValue(oldValue, newValue) {
     for (let i = 0; i < stdArr.length; i++) {
       console.log(stdArr[i], newValue)
       if (stdArr[i] === newValue){
@@ -89,10 +58,21 @@ function App() {
     if (oldValue !== 0) {
       stdArr.push(oldValue)
     }
-
-    // 2. Pass the new array with the value removed to setStrArr() to trigger a re-render
     setStdArr([...stdArr])
   }
+
+  // COMPUTE RACE BONUS HERE
+  const raceBonus = {
+    Strength: 0,
+    Dexterirty: 0,
+    Constitution: 0,
+    Intelligence: 0,
+    Wisdom: 1,
+    Charisma: 0,
+  }
+
+  const races = Object.keys(races); // [ "Human", "Elf", "Half-Orc" ]
+
   return (
     <div className="App">
       <header className="App-header">
@@ -110,18 +90,13 @@ function App() {
         </a>
 
         <nav className="attributes">
-          <Attribute name="Strength" options={stdArr} onSelect={claimValue} />
-          <Attribute name="Dexterirty" options={stdArr} onSelect={claimValue} />
-          <Attribute name="Constitution" options={stdArr} onSelect={claimValue} />
-          <Attribute name="Intelligence" options={stdArr} onSelect={claimValue} />
-          <Attribute name="Wisdom" options={stdArr} onSelect={claimValue} />
-          <Attribute name="Charisma" options={stdArr} onSelect={claimValue} />
+          <Attribute name="Strength" options={stdArr} onSelect={claimValue} bonus={raceBonus.Strength}/>
+          <Attribute name="Dexterirty" options={stdArr} onSelect={claimValue} bonus={raceBonus.Dexterirty} />
+          <Attribute name="Constitution" options={stdArr} onSelect={claimValue} bonus={raceBonus.Constitution} />
+          <Attribute name="Intelligence" options={stdArr} onSelect={claimValue} bonus={raceBonus.Intelligence}/>
+          <Attribute name="Wisdom" options={stdArr} onSelect={claimValue} bonus={raceBonus.Wisdom}/>
+          <Attribute name="Charisma" options={stdArr} onSelect={claimValue} bonus={raceBonus.Charisma} />
         </nav>
-
-
-
-
-
 
 
 
